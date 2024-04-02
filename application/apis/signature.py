dc198426339d48b311b0e6f8709244ce50b7ca71
taku ikegami <takuron1996@gmail.com>
2024-04-03 00:25:01 +0900

    todayにより日付がずれてしまっていたので修正

--- application/apis/signature.py
+++ application/apis/signature.py
@@ -11,7 +11,6 @@ from config.environment import settings
 def create_signature(
     method,
     url,
-    now=datetime.now(ZoneInfo("Asia/Tokyo")).today().strftime("%Y-%m-%d"),
 ) -> str:
     """指定されたHTTPメソッド、URL、および日付を用いて、APIリクエストの署名を生成します。.
 
@@ -22,11 +21,11 @@ def create_signature(
     Args:
         method (str): HTTPリクエストメソッド（例：'GET', 'POST'）。
         url (str): リクエストされるリソースのURL。
-        now (str, optional): 署名の生成に使用される日付。
         デフォルトでは現在の日付（東京のタイムゾーン）。
     Returns:
         str: 生成された署名（16進数の文字列）。.
     """
+    now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d")
     # 正規リクエストの作成
     canonical_request = f"{method}\n{url}\n{now}"
     # 正規リクエストのハッシュ化
