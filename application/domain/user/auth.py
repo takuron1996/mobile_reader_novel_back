5a2de85e9f3dd070baff75eb8fb3ce3bffdec7e2
taku ikegami <takuron1996@gmail.com>
2024-04-04 23:05:39 +0900

    変数名を修正

--- application/domain/user/auth.py
+++ application/domain/user/auth.py
@@ -16,9 +16,9 @@ from schemas.user import AuthUserResponse
 async def create_token(db: AsyncSession, user_id: str) -> AuthUserResponse:
     """アクセストークン及びリフレッシュトークンを生成する関数."""
     minutes = timedelta(minutes=jwt_settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
-    month = timedelta(days=jwt_settings.JWT_REFRESH_TOKEN_EXPIRE_DAYS)
+    days = timedelta(days=jwt_settings.JWT_REFRESH_TOKEN_EXPIRE_DAYS)
     expire = datetime.now(ZoneInfo("Asia/Tokyo")) + minutes
-    refresh_expire = datetime.now(ZoneInfo("Asia/Tokyo")) + month
+    refresh_expire = datetime.now(ZoneInfo("Asia/Tokyo")) + days
     access_token = jwt.encode(
         {"sub": user_id, "exp": expire},
         jwt_settings.JWT_SECRET_ACCESS_KEY,
