13f8e72a3814242889945df696613c2619ce8625
taku ikegami <takuron1996@gmail.com>
2024-04-06 20:32:35 +0900

    エラー発生時用のカスタム例外クラスを作成

---
+++ application/apis/exception.py
@@ -0,0 +1,12 @@
+"""カスタムエラークラス用のモジュール."""
+
+from fastapi import HTTPException
+
+
+class ErrorHttpException(HTTPException):
+    """エラー発生時のカスタム例外クラス."""
+
+    def __init__(self, status_code: int, error: str, error_description: str):
+        """初期化"""
+        detail = {"error": error, "error_description": error_description}
+        super().__init__(status_code=status_code, detail=detail)
