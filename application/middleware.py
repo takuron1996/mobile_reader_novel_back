78041be28c4e4b4c2f850c26da27f14dabfe8f9e
taku ikegami <takuron1996@gmail.com>
2024-04-06 11:30:29 +0900

    例外発生時はHTTPExceptionを使用するように統一

--- application/middleware.py
+++ application/middleware.py
@@ -1,9 +1,8 @@
 """ミドルウェア用のモジュール."""
 import hmac
 
-from fastapi import Request, Response, status
+from fastapi import Request, Response, status, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import JSONResponse
 
 from apis.signature import create_signature
 
@@ -13,13 +12,13 @@ class SignatureMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next) -> Response:
         """ミドルウェアの処理."""
-        error_response = JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={
-                "error": "signature_error",
-                "error_description": "署名が間違っています。",
-            },
-        )
+        error_exception = HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={
+                    "error": "signature_error",
+                    "error_description": "署名が間違っています。",
+                },
+            )
 
         # SwaggerUIへのアクセスだけ除外
         if request.url.path in {"/docs", "/openapi.json"}:
@@ -28,7 +27,7 @@ class SignatureMiddleware(BaseHTTPMiddleware):
         # 署名検証
         signature = request.headers.get("Signature")
         if signature is None:
-            return error_response
+            raise error_exception
 
         url = request.url
         if not hmac.compare_digest(
@@ -37,6 +36,6 @@ class SignatureMiddleware(BaseHTTPMiddleware):
                 request.method, f"{url.scheme}://{url.netloc}{url.path}"
             ),
         ):
-            return error_response
+            raise error_exception
 
         return await call_next(request)
