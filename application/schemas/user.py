300c5c82152222f75b0400853563c18e2bc37de8
taku ikegami <takuron1996@gmail.com>
2024-04-13 11:12:13 +0900

    ユーザー登録APIで使用するschemasを追加

--- application/schemas/user.py
+++ application/schemas/user.py
@@ -1,57 +1,41 @@
-"""ユーザ関連のスキーマ用モジュール."""
+"""ユーザーAPI関連のスキーマ用モジュール."""
 
-from enum import Enum
+import re
 
-from pydantic import BaseModel, Field
+from email_validator import EmailNotValidError, validate_email
+from pydantic import BaseModel, Field, field_validator
 
-
-class GrantType(str, Enum):
-    """認証に使う情報の種類を表すEnumクラス.
-
-    - PASSWORD: ID・passwordによる認証を行うという意味
-    - REFRESH_TOKEN: refresh_tokenによる認証を行うという意味
-    """
-
-    PASSWORD: str = "password"
-    REFRESH_TOKEN: str = "refresh_token"
+PASSWORD_PATTERN = re.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$")
 
 
-class AuthUserModel(BaseModel):
-    """トークンAPIで使用するバリデーションモデル."""
+class UserRegistrationModel(BaseModel):
+    """ユーザーAPIで使用するバリデーションモデル."""
 
-    id: str | None = Field(
+    email: str = Field(
         title="メールアドレス",
         description="ユーザー自身が設定したメールアドレス",
         example="test01@example.com",
-        default=None,
     )
-    password: str | None = Field(
+    password: str = Field(
         title="パスワード",
         description="パスワード",
-        example="pass",
-        default=None,
-    )
-    refresh_token: str | None = Field(
-        title="リフレッシュトークン",
-        description="リフレッシュトークン",
-        example="sdhjjhjhsasfak",
-        default=None,
-    )
-    grant_type: str = Field(
-        ..., title="認証方式", description="認証方式", example="password"
+        example="Password1",
     )
 
+    @field_validator("email")
+    def validate_email(cls, value):
+        """emailのカスタムバリデータ."""
+        try:
+            emailinfo = validate_email(value, check_deliverability=False)
+            email = emailinfo.normalized
+        except EmailNotValidError as exc:
+            print(exc)
+            raise ValueError("指定されたemailは使用することはできません")
+        return email
 
-class AuthUserResponse(BaseModel):
-    """トークンAPIのレスポンス用モデル."""
-
-    access_token: str = Field(
-        title="アクセストークン",
-        description="アクセストークン",
-        example="eyJhbGciO5cCI6IkpXVCJ9.eyJzdWIiOiIxqxLYGqmuL1Pw",
-    )
-    refresh_token: str = Field(
-        title="リフレッシュトークン",
-        description="リフレッシュトークン",
-        example="dsfhgfsfa,.fklsijashkhiafeyehjoszxcsa/j;w",
-    )
+    @field_validator("password")
+    def validate_password(cls, value):
+        """パスワードのカスタムバリデータ."""
+        if not PASSWORD_PATTERN.fullmatch(value):
+            raise ValueError("パスワードの要件を満たしていません")
+        return value
